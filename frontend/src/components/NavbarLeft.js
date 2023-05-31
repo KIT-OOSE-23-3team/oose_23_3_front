773f3e8e1599ad03235da48e81aac0d43c47b1bf
kimjeongmin <kij9451@naver.com>
2023-05-31 10:08:46 +0900

    자전거 관리 화면

---
+++ frontend/src/components/NavbarLeft.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NavbarLeft = () => {
+  return (
+    <div className="navbar-left">
+      <ul>
+        <li>자전거 등록</li>
+        <li>자전거 조회</li>
+        <li>대여소 등록</li>
+        <li>대여소 조회</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavbarLeft;
