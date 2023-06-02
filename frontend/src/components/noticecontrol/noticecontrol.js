a7c4e3ffe09e67fc6106673d39348cc08ac0de6f
Koojunghun <118351019+Koojunghun@users.noreply.github.com>
2023-06-02 15:07:07 +0900

    공지사항 등록 axios 사용 추가

--- frontend/src/components/noticecontrol/noticecontrol.js
+++ frontend/src/components/noticecontrol/noticecontrol.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const addCloseButton = () => {
   const myNodelist = document.getElementsByTagName("LI");
   for (let i = 0; i < myNodelist.length; i++) {
@@ -8,7 +10,6 @@ const addCloseButton = () => {
     myNodelist[i].appendChild(span);
   }
 };
-
 
 const addCheckedToggle = () => {
   const list = document.querySelector('ul');
@@ -21,24 +22,40 @@ const addCheckedToggle = () => {
 
 const newPost = () => {
   const li = document.createElement("li");
-  const titleValue = document.getElementById("myTitleInput").value;
-  const contentValue = document.getElementById("myContentInput").value;
-  const text = document.createTextNode(`${titleValue}: ${contentValue}`);
-  li.appendChild(text);
-  if (titleValue === '' || contentValue === '') {
+  const title = document.getElementById("myTitleInput").value;
+  const text = document.getElementById("myContentInput").value;
+  const postText = document.createTextNode(`${title}: ${text}`);
+  li.appendChild(postText);
+  if (title === '' || text === '') {
     alert("제목과 내용은 반드시 입력해야합니다.");
   } else {
     document.getElementById("myUL").appendChild(li);
     document.getElementById("myTitleInput").value = "";
     document.getElementById("myContentInput").value = "";
+
+    const notice = {
+      title: title,
+      text: text
+    };
+
+    axios.post('/postNotice', notice)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
 };
 
-document.addEventListener("DOMContentLoaded", function() {
+const initializeApp = () => {
   addCloseButton();
   addCheckedToggle();
 
   const registrationBtn = document.getElementById("registration");
   registrationBtn.addEventListener("click", newPost);
-});
+};
 
+document.addEventListener("DOMContentLoaded", initializeApp);
+
+export { addCloseButton, addCheckedToggle, newPost, initializeApp };
