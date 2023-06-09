de6107253ce6edc147c4bdf23f7aff26c10d3371
ikellllllll <shkm1420@naver.com>
2023-06-09 12:33:23 +0900

    [Rental :: 페이지 구현 및 통신 기능 추가(테스트 필수)]

--- frontend/src/components/rental/BicycleHistoryCheck.js
+++ frontend/src/components/rental/BicycleHistoryCheck.js
@@ -1,11 +1,75 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function BicycleHistoryCheck() {
+  const [bicycleNum, setBicycleNum] = useState();
+  const [bicycleList, setBicycleList] = useState([]);
+  const [history, setHistory] = useState([]);
 
-    // return (
+  var userId = "";
+  const getBicycleHistory = () => {
+    axios
+      .get(`http://localhost:8000/bicycleHistoryCheck/${userId}`, { withCredentials: true })
+      .then((r) => {
+        console.log(r);
+        setHistory(r.data);
+      });
+  };
 
-    // );
+  const setList = () => {
+    const updatedList = history.map((item) => item.bicycleNum);
+    setBicycleList(updatedList);
+  };
+
+  useEffect(() => {
+    getBicycleHistory();
+    setList();
+  }, []);
+
+  const handleBicycleListChange = (e) => {
+    setBicycleNum(e.target.value);
+    getBicycleHistory(bicycleNum);
+  };
+
+  return (
+    <div className="create-container">
+      <div className="input-container">
+        <label htmlFor="bicycleList" className="create-input-label">
+          자전거 번호 목록
+        </label>
+        <select
+          className="bicycleList"
+          multiple
+          onChange={handleBicycleListChange}
+        >
+          {bicycleList.map((option) => {
+            <option key={option}>{option}</option>;
+          })}
+        </select>
+      </div>
+      <div className="input-container">
+        <label className="create-input-label">자전거 번호</label>
+        <label>{history.bicycleNum}</label>
+        <label className="create-input-label">결제 금액</label>
+        <label>{history.amount}</label>
+      </div>
+      <div className="input-container">
+        <label className="create-input-label">이용 시간</label>
+        <label>{history.time}</label>
+      </div>
+      <div className="input-container">
+        <label className="create-input-label">대여소</label>
+        <label>{history.rentalOffice}</label>
+        <label className="create-input-label">반납 대여소</label>
+        <label>{history.returnOffice}</label>
+      </div>
+      <div className="input-container">
+        <label className="create-input-label">주행거리</label>
+        <label>{history.mileage}</label>
+      </div>
+      <input type="button" value="확인"></input>
+    </div>
+  );
 }
 
 export default BicycleHistoryCheck;
--- frontend/src/components/rental/rental.js
+++ frontend/src/components/rental/rental.js
@@ -1,9 +1,85 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Rental() {
-    
+  const [bicycleNum, setBicycleNum] = useState();
+  const [office, setOffice] = useState();
+  const [bicycleOptions, setBicycleOptions] = useState([]);
+  const [officeOptions, setOfficeOptions] = useState([]);
 
+  const getOffice = () => {
+    axios.get("http://localhost:8000/rentalOfficeFindAll").then((r) => {
+      console.log(r);
+      setOfficeOptions(r.data);
+    });
+  };
+
+  const getBicycle = () => {
+    axios.get(`http://localhost:8000/bicycleSearch/${office}`).then((r) => {
+      console.log(r);
+      setBicycleOptions(r.data);
+    });
+  };
+
+  useEffect(() => {
+    getOffice();
+  }, []);
+
+  useEffect(() => {
+    if (office) {
+      getBicycle();
+    }
+  }, [office]);
+
+  const rental = () => {
+    axios.get(`http://localhost:8000/bicycleRental/${bicycleNum}`, { withCredentials: true }).then((r) => {
+      console.log(r);
+
+      if (!r) {
+        alert("대여가 정상적으로 되지 않았습니다!");
+      }
+    });
+  };
+
+  return (
+    <div className="create-container">
+      <h1>자전거 대여</h1>
+      <div className="input-container">{/* <mapAPI /> */}</div>
+      <div className="input-container">
+        <label htmlFor="bicycleNum" className="create-input-label">
+          자전거 번호
+        </label>
+        <div className="inputBox">
+          <select
+            className="bicycleNum"
+            value={bicycleNum}
+            onChange={(e) => setBicycleNum(e.target.value)}
+          >
+            {bicycleOptions.map((option) => (
+              <option key={option}>{option}</option>
+            ))}
+          </select>
+        </div>
+      </div>
+      <div className="input-container">
+        <label htmlFor="office" className="create-input-label">
+          대여소
+        </label>
+        <div className="inputBox">
+          <select
+            className="office"
+            value={office}
+            onChange={(e) => setOffice(e.target.value)}
+          >
+            {officeOptions.map((option) => (
+              <option key={option}>{option}</option>
+            ))}
+          </select>
+        </div>
+      </div>
+      <input type="submit" value="대여" onClick={rental}></input>
+    </div>
+  );
 }
 
 export default Rental;
