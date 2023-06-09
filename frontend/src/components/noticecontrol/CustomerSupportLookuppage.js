68978b59ac305e5ff66c49184c7d3d9c0f62fd8a
winteeeee <b12e34e56@naver.com>
2023-06-09 18:36:52 +0900

    [프론트엔드 :: 14주차 PR 내용 확인]

--- frontend/src/components/noticecontrol/CustomerSupportLookuppage.js
+++ frontend/src/components/noticecontrol/CustomerSupportLookuppage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ManagerSupportControl = () => {
+const CustomerSupportLookuppage = () => {
   const [notices, setNotices] = useState([]);
   const [selectedNotice, setSelectedNotice] = useState(null);
 
